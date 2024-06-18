91943bd9cdd11ceb1a95958ebab481c0a28941df
Andrew Richter <andrewrichter1@hotmail.com>
2024-06-17 23:40:51 -0300

    version 1 finished

--- character.py
+++ character.py
@@ -1,4 +1,5 @@
 from weapon import fists
+from health_bar import HealthBar
 
 class Character:
     def __init__(self, name: str, health: int) -> None:
@@ -11,3 +12,25 @@ class Character:
     def attack(self, target) -> None:
         target.health -= self.weapon.damage
         target.health = max(target.health, 0)
+        target.healt_bar.update()
+        print(f'{self.name} dealt {self.weapon.damage} with {self.weapon.name} to {target.name}')
+        
+class Hero(Character):
+    def __init__(self, name: str, health: int):
+        super().__init__(name=name, health=health)
+        self.default_weapon = self.weapon
+        self.healt_bar = HealthBar(self, color="green")
+        
+    def equip(self, weapon) -> None:
+        self.weapon = weapon
+        print(f"{self.name} equipped {self.weapon.name}")
+        
+    def drop(self) -> None:
+        print(f"{self.name} dropped {self.weapon.name}")
+        self.weapon = self.default_weapon
+
+class Enemy(Character):
+    def __init__(self, name: str, health: int, weapon):
+        super().__init__(name=name, health=health)
+        self.healt_bar = HealthBar(self, color="red")
+        self.weapon = weapon
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from character import Character
+from character import Hero, Enemy
+from weapon import short_bow, iron_sword
+import os
 
-hero = Character(name="Hero", health=100)
-enemy = Character(name="Enemy", health=100)
+
+hero = Hero(name="Hero", health=100)
+hero.equip(iron_sword)
+enemy = Enemy(name="Enemy", health=100, weapon=short_bow)
 
 if __name__ == "__main__":
     while True:
+        os.system("cls")
+        
         hero.attack(enemy)
         enemy.attack(hero)
 
-        print(f"Health of {hero.name}: {hero.health}")
-        print(f"Health of {enemy.name}: {enemy.health}")
+        hero.healt_bar.draw()
+        enemy.healt_bar.draw()
+        hero.drop()
 
         input()
